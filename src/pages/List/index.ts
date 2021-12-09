16106c80cf8843f3c37de79646841472453fb0ce
zhangbo2021 <zhangbo819@163.com>
2021-12-09 10:24:43 +0800

    refactor(comp): dir change

--- src/pages/List/index.ts
+++ src/pages/List/index.ts
@@ -1,7 +1,41 @@
 import { createPage } from '@mpxjs/core'
 
+const KEY = "MPX_BILL_KEY"
+type dataItem = string
+
 createPage({
-  onLoad () {
+  onLoad() {
     // onLoad
+  },
+  data: {
+    datas: [],
+    original: []
+  },
+  created() {
+    wx.getStorage({
+      key: KEY,
+      success: (res) => {
+        console.log(res.data);
+        this.setData({
+          datas: res.data.reverse().map((i: dataItem) => i.split('@')),
+          original: res.data
+        })
+      }
+    })
+  },
+  methods: {
+    handleClear() {
+      wx.setStorage({
+        key: KEY,
+        data: []
+      })
+      this.setData({
+        datas: [],
+        original: []
+      })
+    },
+    handleCopy() {
+      wx.setClipboardData({ data: this.original.map(i => `"${i}"`).join(', \n') })
+    }
   }
 })
